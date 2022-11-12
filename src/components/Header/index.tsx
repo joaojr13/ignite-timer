5633679b3e8ef5380a0ba6d65a0fa14b9d26e33d
João Junior <joao.jr13@icloud.com>
2022-11-12 02:37:58 -0300

    feat: criação do Header da aplicação

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" title="Timer" />
+      <nav>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
