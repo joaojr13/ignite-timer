cc710b7d86616fd58ff216d7f6284ff2fa59ace6
João Junior <joao.jr13@icloud.com>
2022-11-12 02:37:09 -0300

    feat: implementação das rotas no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+
+import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
@@ -7,12 +9,9 @@ import { defaultTheme } from './styles/themes/default'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
